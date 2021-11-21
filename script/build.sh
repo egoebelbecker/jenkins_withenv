ca9aeadcdba1d9a0a71299e10f786db59fa78a40
Eric Goebelbecker <eric@ericgoebelbecker.com>
2021-11-21 07:14:01 -0500

    Making script more interesting

--- script/build.sh
+++ script/build.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+ls -l
+
 echo $VALUE > $FILENAME
 
+echo "Created $FILENAME"
+
+ls -l
+
+echo "Contents of $FILENAME:"
 cat $FILENAME
 
+echo "Deleting $FILENAME"
 rm $FILENAME
 
-ls
+ls -l
 
