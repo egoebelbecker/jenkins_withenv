ebcfd7aa39f485acd6976a3b1faf251c5f3d6223
Eric Goebelbecker <eric@ericgoebelbecker.com>
2021-11-21 07:27:28 -0500

    Robustify.

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+#
+# This script requires two env variables:
+# $VALUE - a value to put into a file
+# $FILENAME - the file to put it into
+#
+
+if [ -z "VALUE" ]; then
+    echo "Need to set VALUE"
+    exit 1
+fi
+
+if [ -z "FILENAME" ]; then
+    echo "Need to set FILENAME"
+    exit 1
+fi
+
 
 ls -l
 
